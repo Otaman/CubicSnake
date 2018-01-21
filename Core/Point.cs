f8740bb833a86ad1d309d76fda4ae0d969705ff8
Serhiy Buta <otaman963@gmail.com>
2018-01-21 09:44:02 +0200

    Added Point class

---
+++ Core/Point.cs
@@ -0,0 +1,23 @@
+﻿namespace CubicSnake.Core
+{
+    /// <summary>
+    /// Point represents single point in 3d space
+    /// </summary>
+    /// <typeparam name="T">
+    /// Type for storing point coordinates
+    /// Constraint struct type only for ability use ValuteType.Equals fast byte-to-byte comparison
+    /// </typeparam>
+    public struct Point<T> where T : struct 
+    {
+        public Point(T x, T y, T z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public T X { get; }
+        public T Y { get; }
+        public T Z { get; }
+    }
+}
