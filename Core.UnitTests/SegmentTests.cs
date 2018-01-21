89306abf530b8157bdc0a9c7bbe29a50e585f5f5
Serhiy Buta <otaman963@gmail.com>
2018-01-21 23:22:50 +0200

    Added not fully functional Rotate method for segment

--- Core.UnitTests/SegmentTests.cs
+++ Core.UnitTests/SegmentTests.cs
@@ -13,5 +13,16 @@ namespace CubicSnake.Core.UnitTests
 
             Assert.Throws<ArgumentException>(() => { new Segment(point1, point2); });
         }
+
+        [Fact]
+        public void Rotate_SegmentOnYOneRightByXAxcis_ReturnsSegmentOnZMinusOne()
+        {
+            var axcisSegment = new Segment(new Point(0,0,0), new Point(1, 0, 0));
+            var inputSegment = new Segment(new Point(0,0,0), new Point(0, 1, 0));
+
+            var resultSegment = inputSegment.Rotate(axcisSegment, 90);
+            
+            Assert.True(resultSegment.Head == new Point(0, 0, 1) && resultSegment.Tail == new Point(0,0,0));
+        }
     }
 }
