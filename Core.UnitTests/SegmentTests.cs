14563493c77e22bc4725129e6aa9e83567c34b53
Serhiy Buta <otaman963@gmail.com>
2018-01-21 16:13:38 +0200

    Added Segment definition

---
+++ Core.UnitTests/SegmentTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace CubicSnake.Core.UnitTests
+{
+    public class SegmentTests
+    {
+        [Fact]
+        public void Constructor_EqualsPoints_ThrowException()
+        {
+            var point1 = new Point(1,1,1);
+            var point2 = new Point(1,1,1);
+
+            Assert.Throws<ArgumentException>(() => { new Segment(point1, point2); });
+        }
+    }
+}
---
+++ Core/Segment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CubicSnake.Core
+{
+    /// <summary>
+    /// Segment is straight line between two points with direction from tail to head
+    /// </summary>
+    public struct Segment
+    {
+        public Segment(Point tail, Point head)
+        {
+            if (tail == head)
+                throw new ArgumentException("Tail and head must be not equals");
+            
+            Tail = tail;
+            Head = head;
+        }
+
+        public Point Tail { get; }
+        public Point Head { get; }
+    }
+}
